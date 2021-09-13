6c553af65dfc7f4f21c5cb2b6f38b6efff594e4d
OmegaXking <90558628+OmegaXking@users.noreply.github.com>
2021-09-13 11:55:28 +0530

    Add files via upload

---
+++ AxeVcPlayer/__main__.py
@@ -0,0 +1,23 @@
+
+
+import requests
+from pyrogram import Client as Bot
+
+from AxeVcPlayer.config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
+from AxeVcPlayer.services.callsmusic import run
+
+response = requests.get(BG_IMAGE)
+file = open("./etc/foreground.png", "wb")
+file.write(response.content)
+file.close()
+
+bot = Bot(
+    ":memory:",
+    API_ID,
+    API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="AxeVcPlayer.modules"),
+)
+
+bot.start()
+run()
