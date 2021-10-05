6cb6dea6396ada2ed3e1a3c2a47099379cc46902
Omega <90558628+OmegaXking@users.noreply.github.com>
2021-10-05 22:44:06 +0530

    Update __main__.py

--- AxeVcPlayer/__main__.py
+++ AxeVcPlayer/__main__.py
@@ -1,6 +1,6 @@
 import requests
 from pyrogram import Client as Bot
-from pyrogram import idle
+from pyrogram import idel
 from AxeVcPlayer.config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
 from AxeVcPlayer.services.callsmusic import run
 
