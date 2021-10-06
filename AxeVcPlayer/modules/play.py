92441f77dd93616d2daf81c09e84d96b25877e45
Omega <90558628+OmegaXking@users.noreply.github.com>
2021-10-06 13:00:23 +0530

    Update play.py

--- AxeVcPlayer/modules/play.py
+++ AxeVcPlayer/modules/play.py
@@ -8,7 +8,7 @@ import aiohttp
 import ffmpeg
 import requests
 import wget
-from pytgcalls import clint
+from pytgcalls import client
 from pytgcalls import PyTgCalls
 from pytgcalls import StreamType
 from pytgcalls.types.input_stream import AudioParameters
